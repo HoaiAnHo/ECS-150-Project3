ca2ac25c286e9ed4cf168ad5d3ad11462a905504
HoaiAnHo <hoaianho3100@gmail.com>
2022-11-13 19:36:04 -0800

    Update fs.c

--- p3/libfs/fs.c
+++ p3/libfs/fs.c
@@ -52,7 +52,7 @@ struct root_blocks{
 struct disk_blocks{
 	struct super_block super;
 	struct fat_blocks * fat_front; //starting block
-	struct fat_blocks * fat_back; //starting block
+	struct fat_blocks * fat_back; //ending block
 	struct root_blocks root;
 };
 
@@ -174,8 +174,8 @@ int fs_info(void)
 	printf("rdir_blk=%i\n", cur_disk.super.root_dir_idx);
 	printf("data_blk=%i\n", cur_disk.super.data_blk_idx);
 	printf("data_blk_count=%i\n", cur_disk.super.total_data_blks);
-	printf("fat_free_ratio=%i/%i\n", 800, cur_disk.super.total_data_blks); // WHAT????
-	printf("rdir_free_ratio=%i/%i\n", 100, 128);                           // WHAT????
+	printf("fat_free_ratio=%i/%i\n", free_fats(), cur_disk.super.total_data_blks); // WHAT????
+	printf("rdir_free_ratio=%i/%i\n", free_roots(), 128);                          // WHAT????
 
 	return 0;
 }
@@ -275,6 +275,24 @@ int fs_read(int fd, void *buf, size_t count)
 
 	/* Extend file if necessary */
 }
+
+// helper functions for phase 1
+int free_fats()
+{
+	int free_count = 0;
+	// read through fat blocks, look at fat entries
+	// return how many fat entries are still free
+	return free_count;
+}
+
+int free_roots()
+{
+	int free_count = 0;
+	return free_count;
+}
+
+// helper functions for phase 2
+
 
 // helper functions for phase 4
 // int data_blk_index()
